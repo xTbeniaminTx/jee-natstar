cefde18ec10685b1e67042eda9a623d52c0f4305
xTbeniaminTx <beniamin.tolan@gmail.com>
2022-02-23 11:42:25 +0100

    entity added

--- tp-dataservice/src/main/java/fr/natsystem/tp/data/models/Region.java
+++ tp-dataservice/src/main/java/fr/natsystem/tp/data/models/Region.java
@@ -1,5 +1,39 @@
 package fr.natsystem.tp.data.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "T_REGION")
 public class Region {
+
+	@Id
+	@Column(name = "id")
+	private Long id;
+
+	@NotNull
+	@Column(name = "nom")
+	@Size(min = 1, max = 255, message = "La taille du nom est limite a 255 est min 1")
+	private String nom;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
 }
--- tp-dataservice/src/test/java/fr/natsystem/tp/test/dataservice/SaveRegionTest.java
+++ tp-dataservice/src/test/java/fr/natsystem/tp/test/dataservice/SaveRegionTest.java
@@ -1,5 +1,45 @@
 package fr.natsystem.tp.test.dataservice;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "/")
+
 public class SaveRegionTest {
+	
+	@Test
+	public void test01() {
+		var name = 10;
+		
+		var nameu = 15;
+		
+		System.out.println("coucou");
+		
+		var toot = name + nameu;
+		
+	
+		
+		assertEquals(20, toot);
+	}
+	
+	@Test
+	public void test02() {
+		var name = 10;
+		
+		var nameu = 10;
+		
+		var toot = name + nameu;
+		
+	
+		
+		assertEquals(20, toot);
+	}
+	
 
 }
