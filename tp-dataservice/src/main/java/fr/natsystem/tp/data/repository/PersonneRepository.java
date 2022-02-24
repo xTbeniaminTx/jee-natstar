7503606e48a81cda57bdfa8dcc525fec6904152d
xTbeniaminTx <beniamin.tolan@gmail.com>
2022-02-24 14:35:34 +0100

    14:35

--- tp-dataservice/src/main/java/fr/natsystem/tp/data/repository/PersonneRepository.java
+++ tp-dataservice/src/main/java/fr/natsystem/tp/data/repository/PersonneRepository.java
@@ -2,6 +2,7 @@ package fr.natsystem.tp.data.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +12,7 @@ import fr.natsystem.tp.data.models.Personne;
 public interface PersonneRepository extends JpaRepository<Personne, IdentitePk> {
 
 	List<Personne> findAll(Specification<Personne> spec);
-
+	
+	List<Personne> findAll(Specification<Personne> spec, Pageable pageable);
+	
 }
